2dad906baf131a6ea5d829fce0a7c6c078051143
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-05-11 22:42:39 +0700

    feat: 🎸 styling login

--- Login.php
+++ Login.php
@@ -4,37 +4,18 @@
 <head>
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <div class="login-content">
-        <form class="login-form" action="auth.php" method="post">
-            <h2 class="title">Welcome</h2>
-            <div class="input-div one">
-                <div class="i">
-                    <i class="fas fa-user"></i>
-                </div>
-                <div class="div">
-                    <!-- <h5>Username</h5> -->
-                    <input class="form-control" type="text" name="username" placeholder="Username" autofocus
-                        autocomplete="off" required>
-                </div>
+    <div class="login-content w-full h-full min-h-[100vh] flex items-center justify-center">
+        <form class="login-form flex flex-col items-center space-y-4" action="auth.php" method="post">
+            <h2 class="title text-3xl">Welcome</h2>
+            <div class="flex flex-col space-y-3">
+            <input class="py-2 px-4 border-[1px] border-black rounded-[8px]" type="text" name="username" placeholder="Username" autofocus autocomplete="off" required>
+            <input class="py-2 px-4 border-[1px] border-black rounded-[8px]" type="password" name="password" placeholder="Password" required>
             </div>
-            <div class="input-div pass">
-                <div class="i">
-                    <i class="fas fa-lock"></i>
-                </div>
-                <div class="div">
-                    <!-- <h5>Password</h5> -->
-                    <input class="form-control" type="password" name="password" placeholder="Password" required>
-                </div>
-            </div>
-            <button class="btn btn-primary btn-block" name="login">Login <i
-                    class="fa fa-sign-in fa-lg"></i></button><br>
-            <!-- <input type="submit" class="btn" value="Login"> -->
-
+            <button class="py-2 px-4 border-[1px] border-black rounded-[8px]" name="login">Login <i class="fa fa-sign-in fa-lg"></i></button><br>
         </form>
     </div>
     </div>
